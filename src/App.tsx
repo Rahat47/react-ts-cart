c578b71625767d65bd9258cf2fb7bee3a02aafe3
Rahat47 <djrayhan8@gmail.com>
2021-06-28 13:33:36 +0600

    sidebar structure added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Drawer, LinearProgress, Grid, Badge } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
 import Item from "./components/Item/Item";
 //Styles
-import { Wrapper } from "./app.styles";
+import { StyledButton, Wrapper } from "./app.styles";
 // Types
 export type CartItemType = {
     id: number;
@@ -20,27 +20,41 @@ const getProducts = async (): Promise<CartItemType[]> =>
     await (await fetch("https://fakestoreapi.com/products")).json();
 
 function App() {
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
+
     const { data, isLoading, error } = useQuery<CartItemType[]>(
         "products",
         getProducts
     );
-
-    console.log(data);
 
     if (isLoading) {
         return <LinearProgress />;
     }
 
     if (error) {
-        return <div>Something went wrong!!!</div>;
+        return <div>Something went wrong!!! </div>;
     }
 
-    const getTotalItems = () => null;
+    const getTotalItems = (items: CartItemType[]) =>
+        items.reduce((acc: number, item: CartItemType) => acc + item.amount, 0);
     const handleAddToCart = (item: CartItemType) => null;
     const handleRemoveFromCart = () => null;
 
     return (
         <Wrapper>
+            <Drawer
+                anchor="right"
+                open={cartOpen}
+                onClose={() => setCartOpen(false)}
+            >
+                Cart goes Here
+            </Drawer>
+            <StyledButton onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={getTotalItems(cartItems)} color="error">
+                    <AddShoppingCart />
+                </Badge>
+            </StyledButton>
             <Grid container spacing={3}>
                 {data?.map(item => (
                     <Grid item key={item.id} xs={12} sm={4}>
